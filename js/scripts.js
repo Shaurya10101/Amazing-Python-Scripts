b38365b9210e59d38e91b7434f21194c8732ee33
Shaurya10101 <shauryagupta8046@gmail.com>
2023-06-10 13:05:16 +0530

    copy-code

--- js/scripts.js
+++ js/scripts.js
@@ -34,22 +34,32 @@ document.addEventListener('DOMContentLoaded', function (event) {
   let search = document.getElementById('search')
   search.addEventListener('keydown', Search)
 
-  function populateCards(scriptNumber, scriptDetails) {
-    let cardHolder = document.getElementById('card-holder')
-    let isHidden = scriptNumber <= 20 ? '' : 'hidden'
-
-    let divHTML = `<div class="col-sm-6 col-md-3 ${isHidden} ">
-                      <div class="card shadow">
-                          <div class="card-body">
-                              <h5 class="card-title">${scriptDetails['name']}</h5>
-                              <p class="card-text">${scriptDetails['summary']}</p>
-                              <p>- <a href="https://github.com/${scriptDetails['author']}">${scriptDetails['author']}</a></p><br>
-                              <a href="https://github.com/avinashkranjan/Amazing-Python-Scripts/blob/master/${scriptDetails['folder']}/${scriptDetails['file']}" class="btn btn-primary">Take Me</a>
-                          </div>
-                      </div>
-                  </div>`
-    cardHolder.innerHTML += divHTML
+  let copyButtons = document.getElementsByClassName('copy-code')
+  for (let i = 0; i < copyButtons.length; i++) {
+    let button = copyButtons[i]
+    button.addEventListener('click', function () {
+      let filePath = this.getAttribute('data-file')
+      copyCodeToFile(filePath)
+    })
   }
+
+  function populateCards(scriptNumber, scriptDetails) {
+
+  let divHTML = `<div class="col-sm-6 col-md-3 ${isHidden} ">
+                    <div class="card shadow">
+                        <div class="card-body">
+                            <h5 class="card-title">${scriptDetails['name']}</h5>
+                            <p class="card-text">${scriptDetails['summary']}</p>
+                            <p>- <a href="https://github.com/${scriptDetails['author']}">${scriptDetails['author']}</a></p><br>
+                            <a href="https://github.com/avinashkranjan/Amazing-Python-Scripts/blob/master/${scriptDetails['folder']}/${scriptDetails['file']}" class="btn btn-primary">Take Me</a>
+                            <button class="btn btn-secondary copy-code-btn" data-file="${scriptDetails['file']}">Copy Code</button>
+                        </div>
+                    </div>
+                </div>`
+
+  cardHolder.innerHTML += divHTML
+}
+
 
   function More() {
     loadMore.style.display = 'none'
@@ -58,6 +68,23 @@ document.addEventListener('DOMContentLoaded', function (event) {
     for (let count = 0; count < cardsCount; count++) {
       cards[count].style.display = 'block'
     }
+  }
+  function copyCodeToFile(filePath) {
+    let request = new XMLHttpRequest()
+    request.open('GET', filePath)
+    request.onreadystatechange = function() {
+      if (request.readyState === 4 && request.status === 200) {
+        let code = request.responseText
+        navigator.clipboard.writeText(code)
+          .then(function() {
+            console.log('Code copied to clipboard!')
+          })
+          .catch(function(error) {
+            console.error('Failed to copy code:', error)
+          })
+      }
+    }
+    request.send()
   }
 
   function Search() {
